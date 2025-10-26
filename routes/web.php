bf0888ab47ccc52453b9314be46ebfee7041fcce
Dulaj Nawodya <dulajnawodya5@gmail.com>
2025-10-26 10:50:41 +0530

    qqqq

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('main');
+});
+
+
+Route::get('/dologin', [App\Http\Controllers\MainController::class, 'index'])->name('dologin');
+Route::post('/login', [App\Http\Controllers\MainController::class, 'login'])->name('login');
+Route::get('/logout', [App\Http\Controllers\MainController::class, 'logout'])->name('logout');
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+Route::get('/add-post', [App\Http\Controllers\PostController::class, 'index'])->name('add-post');
+Route::post('/add-post', [App\Http\Controllers\PostController::class, 'store'])->name('save-post');
